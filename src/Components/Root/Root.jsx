cd75bc469197e1ecf3995f223e4759e00cbb53cb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-05 23:27:13 +0600

    add footer component in root

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom"
+import Footer from "../Footer/Footer"
 
 const Root = () => {
   return (
     <div>
         <Outlet/>
-        <
+        <Footer/>
     </div>
   )
 }
