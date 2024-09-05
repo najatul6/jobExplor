7de9352dfada9609113c19d2a7ce75383d3bf1cf
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-05 23:50:28 +0600

    change header text

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Job Explore</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
