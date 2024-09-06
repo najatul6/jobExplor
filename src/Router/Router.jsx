ade80326cf404226ba0c77c75b26ff0aa8bfcf1d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-06 19:32:56 +0600

    Create Blog component and update router

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,6 +3,7 @@ import Root from "../Components/Root/Root";
 import Home from "../Components/Home/Home";
 import Statistics from "../Components/Statistics/Statistics";
 import AppliedJobs from "../Components/AppliedJobs/AppliedJobs";
+import Blog from "../Components/Blog/Blog";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
         {
           path:"/appliedJobs",
           element:<AppliedJobs/>
+        },
+        {
+          path:"/blog",
+          element:<Blog/>
         }
       ],
     },
