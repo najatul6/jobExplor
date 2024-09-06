2445cfecfbd56d7556e2c6155da68b3ca91a2edb
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-06 19:41:26 +0600

    add go back button in error page

--- src/Components/ErrorElement/ErrorElement.jsx
+++ src/Components/ErrorElement/ErrorElement.jsx
@@ -1,8 +1,10 @@
 
 const ErrorElement = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-        
+    <div className="min-h-screen flex flex-col gap-6 justify-center items-center text-white">
+      <h1 className="text-6xl font-bold">404</h1>
+        <p className="text-3xl">No Data Found</p>
+        <button className="btn text-xl text-white">Go back</button>
     </div>
   )
 }
