00ce5a55ffefd9f6f7fd463e218f4c9ee019b368
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-06 20:07:52 +0600

    change nav button

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const {isOpen, setIsOpen}=useState(true)
   const links = (
     <>
       <li>
@@ -21,7 +23,7 @@ const Header = () => {
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <button onClick={()=>setIsOpen(!isOpen)} tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -36,10 +38,10 @@ const Header = () => {
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
-          </div>
+          </button>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow space-y-5"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-72 p-2 shadow-xl space-y-5"
           >
             {links}
           </ul>
