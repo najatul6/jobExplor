fbad7df7369896b53afab316f84ef2d24666d5d5
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-06 19:45:01 +0600

    Complete error page design

--- src/Components/ErrorElement/ErrorElement.jsx
+++ src/Components/ErrorElement/ErrorElement.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 
 const ErrorElement = () => {
   return (
     <div className="min-h-screen flex flex-col gap-6 justify-center items-center text-white">
       <h1 className="text-6xl font-bold">404</h1>
-        <p className="text-3xl">No Data Found</p>
+      <p className="text-3xl">No Data Found</p>
+      <Link to="/">
         <button className="btn text-xl text-white">Go back</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorElement
+export default ErrorElement;
