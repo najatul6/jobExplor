e4e7e4bf7c273a4d53692062490a71d38b1db350
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-06 19:57:00 +0600

    check applied page

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,7 +1,7 @@
 
 const AppliedJobs = () => {
   return (
-    <div>AppliedJobs</div>
+    <div className="min-h-screen flex justify-center items-center text-7xl text-white">Applied Jobs</div>
   )
 }
 
