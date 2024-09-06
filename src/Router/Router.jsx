67b6e1efba17660f519bcf3afbd2710eb7794dd8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-06 19:28:48 +0600

    create statistics route

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -10,6 +10,10 @@ const router = createBrowserRouter([
         {
             path: "/",
             element: <Home/>,
+        },
+        {
+          path:"/statistics",
+          element:
         }
       ],
     },
