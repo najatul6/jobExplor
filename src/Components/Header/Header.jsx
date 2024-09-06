cd95b49ddd2bd1a270aabb4a707d3cd125454a88
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-06 19:49:51 +0600

    add one more button for applying

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -47,7 +47,7 @@ const Header = () => {
         <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn">Apply Job</a>
       </div>
     </div>
   );
