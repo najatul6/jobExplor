68d30eb60200da16cb77be9d673d1f7cf97ba2a9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-07 20:54:38 +0600

    add categories in home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Banner from "../Banner/Banner"
+import Category from "../Category/Category"
 
 const Home = () => {
   return (
     <div className="min-h-screen flex justify-center items-center text-7xl text-white">
       <Banner/>
+      <Category/>
       Home
+
       </div>
   )
 }
