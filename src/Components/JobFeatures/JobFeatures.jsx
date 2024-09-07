d89e4eda2a3122fe59e58329c0d295494f9ebb7b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-07 21:00:30 +0600

    create shared component

--- src/Components/JobFeatures/JobFeatures.jsx
+++ src/Components/JobFeatures/JobFeatures.jsx
@@ -3,6 +3,7 @@ const JobFeatures = () => {
   return (
     <div>
         <h2>Featured Jobs</h2>
+        <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
     </div>
   )
 }
