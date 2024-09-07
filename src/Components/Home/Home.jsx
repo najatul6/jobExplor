c5aff958141a529259f4c0415e2db18ad5a81344
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-07 20:56:15 +0600

    add job feature in home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner"
 import Category from "../Category/Category"
+import JobFeatures from "../JobFeatures/JobFeatures"
 
 const Home = () => {
   return (
     <div className="min-h-screen flex justify-center items-center text-7xl text-white">
       <Banner/>
       <Category/>
+      <JobFeatures/>
       Home
 
       </div>
