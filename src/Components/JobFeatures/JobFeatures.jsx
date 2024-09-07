345594f103f15b9b6518f6bcad45c42f51cd96c8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-07 21:06:18 +0600

    Added subtitle for job features

--- src/Components/JobFeatures/JobFeatures.jsx
+++ src/Components/JobFeatures/JobFeatures.jsx
@@ -3,7 +3,7 @@ import SectionHeader from "../Shared/SectionHeader/SectionHeader"
 const JobFeatures = () => {
   return (
     <div>
-        <SectionHeader title={"Featured Jobs"}/>
+        <SectionHeader title={"Featured Jobs"} subTitle={"Explore thousands of job opportunities with all the information you need. Its your future"}/>
         <h2>Featured Jobs</h2>
         <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
     </div>
