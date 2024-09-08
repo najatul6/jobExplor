f7041877c73defa97200b872d598543122fcf037
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-08 23:34:24 +0600

    show card in grid view

--- src/Components/JobFeatures/JobFeatures.jsx
+++ src/Components/JobFeatures/JobFeatures.jsx
@@ -17,7 +17,7 @@ const JobFeatures = () => {
           "Explore thousands of job opportunities with all the information you need. Its your future"
         }
       />
-      <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2  gap-6">
         {
           jobs?.map((job) =><JobsCard key={job.id} job={job}/>)
         }
