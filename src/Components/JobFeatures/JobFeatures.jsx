c822ab5153e665a10d55d4a0d63716a551d9db3b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-08 22:56:01 +0600

    get jobs data using fetch

--- src/Components/JobFeatures/JobFeatures.jsx
+++ src/Components/JobFeatures/JobFeatures.jsx
@@ -1,12 +1,28 @@
-import SectionHeader from "../Shared/SectionHeader/SectionHeader"
+import { useEffect, useState } from "react";
+import SectionHeader from "../Shared/SectionHeader/SectionHeader";
 
 const JobFeatures = () => {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
   return (
     <div>
-        <SectionHeader title={"Featured Jobs"} subTitle={"Explore thousands of job opportunities with all the information you need. Its your future"}/>
-        
+      <SectionHeader
+        title={"Featured Jobs"}
+        subTitle={
+          "Explore thousands of job opportunities with all the information you need. Its your future"
+        }
+      />
+      <div>
+        {
+          
+        }
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default JobFeatures
+export default JobFeatures;
