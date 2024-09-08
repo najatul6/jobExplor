0fc8c440ab12e33fa892f43d4ded8910e6c951c6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-08 22:30:12 +0600

    add banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,22 @@
-
+import bannerImg from "../../assets/man.png";
 const Banner = () => {
   return (
-    <div>
-        
+    <div className="grid grid-cols-2 place-items-center mx-auto py-5 w-full">
+      <div className="w-2/3 border space-y-6">
+        <h2 className="text-4xl font-bold capitalize">
+          One Step <br /> Closer To Your <br />{" "}
+          <span className="text-blue-500">Dream Job</span>
+        </h2>
+        <p>
+          Explore thousands of job opportunities with all the information you
+          need. Its your future. Come find it. Manage all your job application
+          from start to finish.
+        </p>
+        <button className="btn bg-blue-500 text-white">Get Started</button>
+      </div>
+        <img src={bannerImg} alt="" className="w-full object-cover" />
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
