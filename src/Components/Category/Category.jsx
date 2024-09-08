900b33638d5c3339d938fb0c7d2e379b800acca0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-08 22:36:19 +0600

    get data using fetch

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react"
 import SectionHeader from "../Shared/SectionHeader/SectionHeader"
+import { useEffect } from "react"
 
 const Category = () => {
+  const [categories,setCategories]=useState([])
+  useEffect(()=>{
+    fetch('categories.json')
+    .then(res=>res.json())
+    .then(data=>setCategories(data))
+  },[])
   return (
     <div>
         <SectionHeader title={"Job Category List"} subTitle={"Explore thousands of job opportunities with all the information you need. Its your future"}/>
+
+          <div className="grid grid-cols-2 gap-5 py-5">
+            {categories.map((cat)=>(
+              <div key={cat.id} className="border p-5 rounded-md shadow-md hover:shadow-2xl">
+                <h3 className="text-xl font-bold">{cat.name}</h3>
+                <p>{cat.description}</p>
+              </div>
+            ))}
+          </div>
     </div>
   )
 }
