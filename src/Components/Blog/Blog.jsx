09493ae188f69338056efd26b8c438eb1c170a46
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-08 23:55:18 +0600

    updating some section in blog components

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -12,7 +12,7 @@ const Blog = () => {
       </header>
 
       {/* Blog Posts  */}
-      <section className="py-12">
+      <section className="py-12 px-5">
         <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div className="bg-white shadow-lg rounded-lg overflow-hidden">
             <img
