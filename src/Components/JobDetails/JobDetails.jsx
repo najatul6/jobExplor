ad24e3a1292f43de1e6025f0ec0b4bd0c8ea2749
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-09 23:17:05 +0600

    update page ui design in job Details

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -4,6 +4,9 @@ const JobDetails = () => {
       <div className="py-20 bg-blue-400 text-white flex flex-col justify-center items-center">
         <h1 className="text-4xl font-bold uppercase">Job Details</h1>
       </div>
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        hello
+      </div>
     </div>
   );
 };
