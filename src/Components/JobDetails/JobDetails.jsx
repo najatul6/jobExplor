5852561eb5b97d1ac426d6b7d66c8268b3d139d3
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-09 23:26:49 +0600

    update size for two sections

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -4,9 +4,9 @@ const JobDetails = () => {
       <div className="py-20 bg-blue-400 text-white flex flex-col justify-center items-center">
         <h1 className="text-4xl font-bold uppercase">Job Details</h1>
       </div>
-      <div className="min-h-screen flex flex-col justify-center items-center">
-        <div className="border-2 w-full border-red-800"></div>
-        <div className="border-2 w-full border-blue-700"></div>
+      <div className="min-h-screen flex justify-center gap-5 items-center py-5 px-5">
+        <div className="border-2 w-2/3 h-screen border-red-800"></div>
+        <div className="border-2 w-1/3 h-screen border-blue-700"></div>
       </div>
     </div>
   );
