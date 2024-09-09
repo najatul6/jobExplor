c7d3c3293923e4439a2758ba3b37ffc4e80606e9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-10 01:49:03 +0600

    align full body for ui user flexiable

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,45 +3,47 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="footer bg-base-200 text-base-content p-10">
-      <aside>
-        <h2 className="text-3xl uppercase font-bold">Job Explore</h2>
-        <p>
-          There are many variations of passages <br /> of Lorem Ipsum , but the
-          majority have <br /> suffered alteration in some form.
-        </p>
-        <div className="grid grid-flow-col gap-4">
-          <Link to="" className="p-3 bg-gray-400 rounded-full text-white btn">
-          <FaFacebookF  className="text-xl"/>
-          </Link>
-          <Link to="" className="p-3 bg-gray-400 rounded-full text-white btn">
-          <FaTwitter className="text-xl" />
-          </Link>
-          <Link to="" className="p-3 bg-gray-400 rounded-full text-white btn">
-          <FaInstagram className="text-xl" />
-          </Link>
-        </div>
-      </aside>
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
+    <footer className=" bg-base-200 text-base-content p-10">
+      <div className="footer max-w-7xl mx-auto">
+        <aside>
+          <h2 className="text-3xl uppercase font-bold">Job Explore</h2>
+          <p>
+            There are many variations of passages <br /> of Lorem Ipsum , but
+            the majority have <br /> suffered alteration in some form.
+          </p>
+          <div className="grid grid-flow-col gap-4">
+            <Link to="" className="p-3 bg-gray-400 rounded-full text-white btn">
+              <FaFacebookF className="text-xl" />
+            </Link>
+            <Link to="" className="p-3 bg-gray-400 rounded-full text-white btn">
+              <FaTwitter className="text-xl" />
+            </Link>
+            <Link to="" className="p-3 bg-gray-400 rounded-full text-white btn">
+              <FaInstagram className="text-xl" />
+            </Link>
+          </div>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+      </div>
     </footer>
   );
 };
--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,15 +1,17 @@
-import { Outlet } from "react-router-dom"
-import Footer from "../Footer/Footer"
-import Header from "../Header/Header"
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 
 const Root = () => {
   return (
     <div>
-        <Header/>
-        <Outlet/>
-        <Footer/>
+      <div className="max-w-7xl mx-auto">
+        <Header />
+        <Outlet />
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
