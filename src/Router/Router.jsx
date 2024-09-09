b5b356631b61e3d7651f76c47a136cb1fe48fce5
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-09 23:18:22 +0600

    add loader for loading data

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -22,8 +22,9 @@ const router = createBrowserRouter([
           element:<Statistics/>
         },
         {
-          path:"/jobDetails/2",
-          element:<JobDetails/>
+          path:"/jobDetails/:id",
+          element:<JobDetails/>,
+          loader:
         },
         {
           path:"/appliedJobs",
