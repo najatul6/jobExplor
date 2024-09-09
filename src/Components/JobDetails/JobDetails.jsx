7c1b3805b7297a7006a2b03d3b0594bcf8bc0c67
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-09 23:27:19 +0600

    add some text in job details section

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -5,8 +5,8 @@ const JobDetails = () => {
         <h1 className="text-4xl font-bold uppercase">Job Details</h1>
       </div>
       <div className="min-h-screen flex justify-center gap-5 items-center py-5 px-5">
-        <div className="border-2 w-2/3 h-screen border-red-800"></div>
-        <div className="border-2 w-1/3 h-screen border-blue-700"></div>
+        <div className="border-2 w-2/3 h-screen border-red-800">Hello Wold</div>
+        <div className="border-2 w-1/3 h-screen border-blue-700">Hello Wold</div>
       </div>
     </div>
   );
