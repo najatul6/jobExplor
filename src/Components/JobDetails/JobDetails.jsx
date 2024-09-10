e0c53a28180292ab02a0eab0e1d91885de4ceec6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-10 18:33:16 +0600

    show all data in ui

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -1,3 +1,8 @@
+import { BsPersonWorkspace } from "react-icons/bs";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { HiOutlineCurrencyDollar } from "react-icons/hi2";
+import { IoMail } from "react-icons/io5";
 import { useLoaderData, useParams } from "react-router-dom";
 
 const JobDetails = () => {
@@ -5,7 +10,7 @@ const JobDetails = () => {
   const { id } = useParams();
   const idInt = parseInt(id);
   const job = jobs.find((job) => job.id === idInt);
-  const { 
+  const {
     job_title,
     company_name,
     remote_or_onsite,
@@ -16,7 +21,10 @@ const JobDetails = () => {
     job_responsibility,
     educational_requirements,
     experiences,
-   } = job;
+    phone,
+    email,
+    address,
+  } = job;
   return (
     <div>
       <div className="py-20 bg-base-200  text-white flex flex-col gap-4 justify-center items-center">
@@ -25,12 +33,51 @@ const JobDetails = () => {
       </div>
       <div className="min-h-screen flex flex-col md:flex-row justify-center gap-5 items-center py-5 px-5">
         <div className="border-2 w-2/3 h-screen border-red-800">
-        <p>
-        <span>Job Description: </span> {job_description}
-        </p>
+          <p>
+            <span>Job Description:</span> {job_description}
+          </p>
+          <p>
+            <span>Job Responsibility:</span> {job_responsibility}
+          </p>
+          <h4>Educational Requirements:</h4>
+          <p>{educational_requirements}</p>
+          <h4>Experiences:</h4>
+          <p>{experiences}</p>
         </div>
         <div className="border-2 w-1/3 h-screen border-blue-700">
-          Hello Wold
+          <div>
+            <h2>Job Details</h2>
+            <hr />
+            <p>
+              <HiOutlineCurrencyDollar />
+              <span>Salary :</span>
+              {salary}
+            </p>
+            <p>
+              <BsPersonWorkspace />
+              <span>Job Title :</span>
+              {job_title}
+            </p>
+          </div>
+          <div>
+            <h2>Contact Information</h2>
+            <hr />
+            <p>
+              <FaPhoneAlt />
+              <span>Phone : </span>
+              {phone}
+            </p>
+            <p>
+              <IoMail />
+              <span>Email : </span>
+              {email}
+            </p>
+            <p>
+              <FaLocationDot />
+              <span>Address : </span>
+              {address}
+            </p>
+          </div>
         </div>
       </div>
     </div>
