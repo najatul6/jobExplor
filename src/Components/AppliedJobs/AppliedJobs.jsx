020252f9f8a3cdc44c7d5b87ca35d003f7483148
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-11 08:30:24 +0600

    add dependency

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../../utility/localStorage";
 
 const AppliedJobs = () => {
-  const jobs= useLoaderData();
-  const [filterdJob,setFilterdJob]=useState()
-  useEffect(()=>{
-const storedAppliedJobs=getStoredJobApplication();
-if(storedAppliedJobs>0){
-  const jobApplied=jobs.filter(job=>job.id===jobApplications)
-}
-  },[])
+  const jobs = useLoaderData();
+  const [filterdJob, setFilterdJob] = useState();
+  useEffect(() => {
+    const storedAppliedJobs = getStoredJobApplication();
+    if (storedAppliedJobs > 0) {
+      const jobApplied = jobs.filter((job) => job.id === jobApplications);
+    }
+  }, [jobs]);
   return (
     <div className=" text-white">
       Applied Jobs
       <h2>Helo world</h2>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default AppliedJobs
+export default AppliedJobs;
