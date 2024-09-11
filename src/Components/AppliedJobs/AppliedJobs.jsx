8b6af74ae21134e909bd7c568b85902f7d242999
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-11 20:40:34 +0600

    Fix problem for import useState

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../../utility/localStorage";
 
