13d2c4b4ada819c2c2efde551053d512a171c191
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-12 20:33:16 +0600

    format all code

--- src/Components/GoogleMap/GoogleMap.jsx
+++ src/Components/GoogleMap/GoogleMap.jsx
@@ -1,33 +1,30 @@
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from "google-map-react";
 import PropTypes from "prop-types";
+
 const GoogleMap = () => {
-    const defaultProps = {
-        center: {
-          lat: 10.99835602,
-          lng: 77.01502627
-        },
-        zoom: 11
-      };
-      const AnyReactComponent = ({ text }) => <div>{text}</div>;
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
+  const AnyReactComponent = ({ text }) => <div>{text}</div>;
   return (
-    <div style={{ height: '300px', width: '100%' }}>
+    <div style={{ height: "300px", width: "100%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: "" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="Our Office"
-        />
+        <AnyReactComponent lat={59.955413} lng={30.337844} text="Our Office" />
       </GoogleMapReact>
     </div>
-  )
-}
+  );
+};
 
 GoogleMap.propTypes = {
-    text:PropTypes.string
-  }
+  text: PropTypes.string,
+};
 
-export default GoogleMap
+export default GoogleMap;
