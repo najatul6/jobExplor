d82e56315f24fe7b1fa3721ad5b57aa8a18d6471
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-12 19:18:37 +0600

    add helmet provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router/Router";
 import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </StrictMode>
 );
