ccb8d0ddb5a6136eeffd70785176206bcbb531a7
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-01 23:27:42 +0600

    remove some class Names

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -8,8 +8,8 @@ const Stats = () => {
             in Real State Industry
         </h1>
         <div className="md:mt-14 mt-4 relative sm:flex items-center justify-center">
-            <img src="https://i.ibb.co/KjrPCyW/map.png" alt="world map image" className="w-full xl:h-full h-96 object-cover object-fill sm:block hidden" />
-            <img src="https://i.ibb.co/SXKj9Mf/map-bg.png" alt="mobile-image" className="sm:hidden -mt-10 block w-full h-96 object-cover object-fill absolute z-0" />
+            <img src="https://i.ibb.co/KjrPCyW/map.png" alt="world map image" className="w-full xl:h-full h-96 object-cover  sm:block hidden" />
+            <img src="https://i.ibb.co/SXKj9Mf/map-bg.png" alt="mobile-image" className="sm:hidden -mt-10 block w-full h-96 object-cover  absolute z-0" />
             <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20 sm:mt-0 mt-4 left-0 xl:ml-56 sm:ml-12 xl:-mt-40 sm:-mt-12">
                 <p className="text-3xl font-semibold text-gray-800">20K+</p>
                 <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">Recently Property Listed</p>
