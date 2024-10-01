34f1b6c0ec339636de0b66da5c0d72a492304b06
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-01 23:24:06 +0600

    modify some sections

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -1,82 +1,26 @@
 
-const Testimonial = () => {
+const Stats = () => {
   return (
     <>
-    <div id="blog" className="bg-gray-100 px-4 xl:px-0 py-12">
-        <div className="mx-auto container">
-            <h1 className="text-center text-3xl lg:text-5xl tracking-wider text-gray-900">Latest from our Blog</h1>
-            <div className="mt-12 lg:mt-24">
-                <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-8">
-                    <div>
-                        <img className="w-full" src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(1).png" alt="computer" />
-                        <div className="py-4 px-8 w-full flex justify-between bg-indigo-700">
-                            <p className="text-sm text-white font-semibold tracking-wide">Bruce Wayne</p>
-                            <p className="text-sm text-white font-semibold tracking-wide">13TH Oct, 2020</p>
-                        </div>
-                        <div className="bg-white px-10 py-6 rounded-bl-3xl rounded-br-3xl">
-                            <h1 className="text-4xl text-gray-900 font-semibold tracking-wider">Transactions</h1>
-                            <p className="text-gray-700 text-base lg:text-lg  lg:leading-8 tracking-wide mt-6 w-11/12">Find the latest events updates or create events, concerts, conferences, workshops, exhibitions, and cultural events in all cities of the US. The aim of Eventistan is to promote healthy and entertaining event.Find the latest events updates or create events, concerts, conferences, workshops, exhibitions, and cultural events in all cities of the US. The aim of Eventistan is to promote healthy and entertaining event.</p>
-                            <div className="w-full mt-4 justify-end flex items-center cursor-pointer">
-                                <p className="text-base tracking-wide text-indigo-500">Read more</p>
-                                <svg className="ml-3 lg:ml-6" xmlns="http://www.w3.org/2000/svg" width={20} height={18} viewBox="0 0 20 18" fill="none">
-                                    <path d="M11.7998 1L18.9998 8.53662L11.7998 16.0732" stroke="#4338ca" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
-                                    <path d="M1 8.53662H19" stroke="#4338ca" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
-                                </svg>
-                            </div>
-                            <div className="h-5 w-2" />
-                        </div>
-                    </div>
-                    <div>
-                        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
-                            <div>
-                                <img className="w-full" src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(2).png" alt="games" />
-                                <div className="py-2 px-4 w-full flex justify-between bg-indigo-700">
-                                    <p className="text-sm text-white font-semibold tracking-wide">Bruce Wayne</p>
-                                    <p className="text-sm text-white font-semibold tracking-wide">13TH Oct, 2020</p>
-                                </div>
-                                <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
-                                    <h1 className="text-lg text-gray-900 font-semibold tracking-wider">Transactions</h1>
-                                    <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">Find the latest events updates or create events, concerts, conferences, workshops...</p>
-                                </div>
-                            </div>
-                            <div>
-                                <img className="w-full" src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(3).png" alt="notes" />
-                                <div className="py-2 px-4 w-full flex justify-between bg-indigo-700">
-                                    <p className="text-sm text-white font-semibold tracking-wide">Bruce Wayne</p>
-                                    <p className="text-sm text-white font-semibold tracking-wide">13TH Oct, 2020</p>
-                                </div>
-                                <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
-                                    <h1 className="text-lg text-gray-900 font-semibold tracking-wider">Transactions</h1>
-                                    <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">Find the latest events updates or create events, concerts, conferences, workshops...</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
-                            <div>
-                                <img className="w-full" src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(4).png" alt="laptop" />
-                                <div className="py-2 px-4 w-full flex justify-between bg-indigo-700">
-                                    <p className="text-sm text-white font-semibold tracking-wide">Bruce Wayne</p>
-                                    <p className="text-sm text-white font-semibold tracking-wide">13TH Oct, 2020</p>
-                                </div>
-                                <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
-                                    <h1 className="text-lg text-gray-900 font-semibold tracking-wider">Transactions</h1>
-                                    <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">Find the latest events updates or create events, concerts, conferences, workshops...</p>
-                                </div>
-                            </div>
-                            <div>
-                                <img className="w-full" src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(5).png" alt="worker" />
-                                <div className="py-2 px-4 w-full flex justify-between bg-indigo-700">
-                                    <p className="text-sm text-white font-semibold tracking-wide">Bruce Wayne</p>
-                                    <p className="text-sm text-white font-semibold tracking-wide">13TH Oct, 2020</p>
-                                </div>
-                                <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
-                                    <h1 className="text-lg text-gray-900 font-semibold tracking-wider">Transactions</h1>
-                                    <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">Find the latest events updates or create events, concerts, conferences, workshops...</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <div className="xl:px-20 px-6 py-20 xl:mx-auto xl:container">
+        <h1 className="xl:text-5xl md:text-4xl text-2xl font-semibold leading-tight text-center text-gray-800 sm:mb-0 mb-12">
+            More Than 10 Years We Provide Service <br className="md:block hidden" />
+            in Real State Industry
+        </h1>
+        <div className="md:mt-14 mt-4 relative sm:flex items-center justify-center">
+            <img src="https://i.ibb.co/KjrPCyW/map.png" alt="world map image" className="w-full xl:h-full h-96 object-cover object-fill sm:block hidden" />
+            <img src="https://i.ibb.co/SXKj9Mf/map-bg.png" alt="mobile-image" className="sm:hidden -mt-10 block w-full h-96 object-cover object-fill absolute z-0" />
+            <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20 sm:mt-0 mt-4 left-0 xl:ml-56 sm:ml-12 xl:-mt-40 sm:-mt-12">
+                <p className="text-3xl font-semibold text-gray-800">20K+</p>
+                <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">Recently Property Listed</p>
+            </div>
+            <div className="shadow-lg xl:p-6 p-4 w-48 sm:w-auto w-full bg-white sm:absolute relative z-20 sm:mt-0 mt-4 xl:mt-80 sm:mt-56 xl:-ml-0 sm:-ml-12">
+                <p className="text-3xl font-semibold text-gray-800">8K+</p>
+                <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">Active Listening</p>
+            </div>
+            <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20 md:mt-0 sm:-mt-5 mt-4 right-0 xl:mr-56 sm:mr-24">
+                <p className="text-3xl font-semibold text-gray-800">15K+</p>
+                <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">Recently Sold Lands</p>
             </div>
         </div>
     </div>
@@ -84,4 +28,4 @@ const Testimonial = () => {
   )
 }
 
-export default Testimonial
+export default Stats
