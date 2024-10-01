7a4e78a03b58206b3eaaa0531e5b50f0a98412c8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-01 23:16:09 +0600

    modify some card

--- src/Components/Faq/Faq.jsx
+++ src/Components/Faq/Faq.jsx
@@ -1,9 +1,9 @@
 const Faq = () => {
   return (
     <>
-      <div className="bg-gray-100">
+      <div className="bg-base-100">
         <div className="container mx-auto">
-          <div role="article" className="bg-gray-100 py-12 md:px-8">
+          <div role="article" className="bg-base-100 py-12 md:px-8">
             <div className="px-4 xl:px-0 py-10">
               <div className="flex flex-col lg:flex-row flex-wrap">
                 <div className="mt-4 lg:mt-0 lg:w-3/5">
@@ -48,12 +48,12 @@ const Faq = () => {
             </div>
             <div className="px-6 xl:px-0">
               <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pb-6 gap-8">
-                <div role="cell" className="bg-gray-100">
+                <div role="cell" className="bg-base-100">
                   <div className="bg-white p-5 rounded-md relative h-full w-full">
                     {/* class="absolute inset-0 object-center object-cover h-full w-full"  */}
                     <span>
                       <img
-                        className="bg-gray-200 p-2 mb-5 rounded-full"
+                        className="bg-base-200 p-2 mb-5 rounded-full"
                         src="https://i.ibb.co/27R6nk5/home-1.png"
                         alt="home-1"
                       />
@@ -178,12 +178,12 @@ const Faq = () => {
                     </a>
                   </div>
                 </div>
-                <div role="cell" className="bg-gray-100">
+                <div role="cell" className="bg-base-100">
                   <div className="bg-white p-5 rounded-md relative h-full w-full">
                     {/* class="absolute inset-0 object-center object-cover h-full w-full"  */}
                     <span>
                       <img
-                        className="bg-gray-200 p-2 mb-5 rounded-full"
+                        className="bg-base-200 p-2 mb-5 rounded-full"
                         src="https://i.ibb.co/bdGyLYk/pricetags-1.png"
                         alt="pricetags-1"
                       />
@@ -332,7 +332,7 @@ const Faq = () => {
                     </a>
                   </div>
                 </div>
-                <div role="cell" className="bg-gray-100">
+                <div role="cell" className="bg-base-100">
                   <div className="bg-white p-5 rounded-md relative h-full w-full">
                     {/* class="absolute inset-0 object-center object-cover h-full w-full"  */}
                     <span>
@@ -464,7 +464,7 @@ const Faq = () => {
                     </a>
                   </div>
                 </div>
-                <div role="cell" className="bg-gray-100">
+                <div role="cell" className="bg-base-100">
                   <div className="bg-white p-5 rounded-md  h-full relative w-full">
                     {/* class="absolute inset-0 object-center object-cover h-full w-full"  */}
                     <span>
@@ -597,7 +597,7 @@ const Faq = () => {
                   </div>
                 </div>
                 <div role="cell" className="bg-gray-100">
-                  <div className="relative bg-white p-5 rounded-md relative h-full w-full">
+                  <div className="relative bg-white p-5 rounded-md h-full w-full">
                     {/* class="absolute inset-0 object-center object-cover h-full w-full"  */}
                     <span>
                       <img
@@ -673,7 +673,7 @@ const Faq = () => {
                           </svg>
                         </div>
                         <h4 className="text-md text-gray-900 dark:text-gray-100 pl-4">
-                          I didnt get a confirmation email, what should I do
+                          I didn&#39;t get a confirmation email, what should I do
                           next
                         </h4>
                       </div>
